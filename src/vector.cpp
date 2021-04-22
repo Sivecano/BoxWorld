565bd0f1dc5212c959aad27067f401cbf441dd40
Sivecano <Sivecano@gmail.com>
2021-04-22 19:50:45 +0200

    add vectors for simpler meth and do VERY HACKY attempts at improving collision

---
+++ src/vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sivecano on 14/04/2021.
+//
+
+#include "vector.h"
---
+++ src/vector.h
@@ -0,0 +1,14 @@
+//
+// Created by sivecano on 14/04/2021.
+//
+
+#ifndef BOXWORLD_VECTOR_H
+#define BOXWORLD_VECTOR_H
+
+
+class vector {
+
+};
+
+
+#endif //BOXWORLD_VECTOR_H
